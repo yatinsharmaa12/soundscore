61420d59f05d52c1ec870b0300852f56bd84da7c
Sidharth-Singh10 <sidharthsingh714@gmail.com>
2024-06-02 04:32:40 +0530

    added_Middleware

--- backend/src/middleware.ts
+++ backend/src/middleware.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
-import { JWT_SECRET } from ".";
+import { JWT_SECRET } from "./config";
+import { WORKER_JWT_SECRET } from "./config";
 
 export function authMiddleware(
   req: Request,
@@ -10,6 +11,34 @@ export function authMiddleware(
   const authHeader = req.headers["authorization"] ?? "";
   try {
     const decoded = jwt.verify(authHeader, JWT_SECRET);
+    //@ts-ignore
+    if (decoded.userId) {
+        //@ts-ignore
+        req.userId  = decoded.userId;
+        return next();
+
+    }
+    else
+    {
+        return res.status(403).json({ message: "NOT Logged SOmetisda IN" });
+
+    }
+
+    next();
+  } catch (err) {
+    return res.status(403).json({ message: "NOT asdasd IN" });
+  }
+}
+
+
+export function workerAuthMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const authHeader = req.headers["authorization"] ?? "";
+  try {
+    const decoded = jwt.verify(authHeader, WORKER_JWT_SECRET);
     //@ts-ignore
     if (decoded.userId) {
         //@ts-ignore
